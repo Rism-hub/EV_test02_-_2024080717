563bfdb6884646765cbe9a249155ede652de2ec9
“Rism-hub” <lini162003@163.com>
2025-04-26 12:08:17 +0800

    ros2

---
+++ ros2_ws/install/py_pubsub/lib/python3.8/site-packages/py_pubsub/subscriber_member_function.py
@@ -0,0 +1,28 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class HelloSubscriber(Node):
+    def __init__(self):
+        super().__init__('hello_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'greeting_topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('2024080717 刘宁')
+        # 如果需要也可以打印接收到的消息内容
+        # self.get_logger().info(f'Received message: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    hello_subscriber = HelloSubscriber()
+    rclpy.spin(hello_subscriber)
+    hello_subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
